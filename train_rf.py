76f83fa50b3deaa76d035e44780bde5ca98e75fc
Nathan Laha <nlaha@outlook.com>
2024-11-20 18:57:57 +0000

    add aggregation for source data

--- train_rf.py
+++ train_rf.py
@@ -25,21 +25,25 @@ import swifter
 
 USE_SOURCE_DATA = True
 
-# Model name
-MODEL_NAME = "xgboost_rf_ten_percent_data"
-
 # Sample percent of the dataset to use
-DATASET_SAMPLE_PERCENT = 0.1
+DATASET_SAMPLE_PERCENT = 0.01
 
-# DROPPED_X_COLS = [
-#     "Label",
-#     "Timestamp",
-#     "Dst Port",
-#     "Dst IP",
-#     "Src Port",
-#     "Src IP",
-#     "Flow ID",
-# ]
+SAMPLING_INTERVAL = '10s'
+
+# Model name
+MODEL_NAME = f"xgboost_rf_{DATASET_SAMPLE_PERCENT}_data_{SAMPLING_INTERVAL}"
+
+TIMESTAMP_COL = "Timestamp"
+
+DROPPED_X_COLS = [
+    "Label",
+    "Timestamp",
+    "Dst Port",
+    "Dst IP",
+    "Src Port",
+    "Src IP",
+    "Flow ID",
+]
 
 # DROPPED_X_COLS = ["date_minutes", "attack_type", "is_attack", "has_attack_ip"]
 
@@ -49,26 +53,26 @@ TARGET = "Label"
 # X_COLS = None
 X_COLS = [
     'Protocol', 'Flow Duration', 'Tot Fwd Pkts',
-       'Tot Bwd Pkts', 'TotLen Fwd Pkts', 'TotLen Bwd Pkts', 'Fwd Pkt Len Max',
-       'Fwd Pkt Len Min', 'Fwd Pkt Len Mean', 'Fwd Pkt Len Std',
-       'Bwd Pkt Len Max', 'Bwd Pkt Len Min', 'Bwd Pkt Len Mean',
-       'Bwd Pkt Len Std', 'Flow Byts/s', 'Flow Pkts/s', 'Flow IAT Mean',
-       'Flow IAT Std', 'Flow IAT Max', 'Flow IAT Min', 'Fwd IAT Tot',
-       'Fwd IAT Mean', 'Fwd IAT Std', 'Fwd IAT Max', 'Fwd IAT Min',
-       'Bwd IAT Tot', 'Bwd IAT Mean', 'Bwd IAT Std', 'Bwd IAT Max',
-       'Bwd IAT Min', 'Fwd PSH Flags', 'Bwd PSH Flags', 'Fwd URG Flags',
-       'Bwd URG Flags', 'Fwd Header Len', 'Bwd Header Len', 'Fwd Pkts/s',
-       'Bwd Pkts/s', 'Pkt Len Min', 'Pkt Len Max', 'Pkt Len Mean',
-       'Pkt Len Std', 'Pkt Len Var', 'FIN Flag Cnt', 'SYN Flag Cnt',
-       'RST Flag Cnt', 'PSH Flag Cnt', 'ACK Flag Cnt', 'URG Flag Cnt',
-       'CWE Flag Count', 'ECE Flag Cnt', 'Down/Up Ratio', 'Pkt Size Avg',
-       'Fwd Seg Size Avg', 'Bwd Seg Size Avg', 'Fwd Byts/b Avg',
-       'Fwd Pkts/b Avg', 'Fwd Blk Rate Avg', 'Bwd Byts/b Avg',
-       'Bwd Pkts/b Avg', 'Bwd Blk Rate Avg', 'Subflow Fwd Pkts',
-       'Subflow Fwd Byts', 'Subflow Bwd Pkts', 'Subflow Bwd Byts',
-       'Init Fwd Win Byts', 'Init Bwd Win Byts', 'Fwd Act Data Pkts',
-       'Fwd Seg Size Min', 'Active Mean', 'Active Std', 'Active Max',
-       'Active Min', 'Idle Mean', 'Idle Std', 'Idle Max', 'Idle Min'
+    'Tot Bwd Pkts', 'TotLen Fwd Pkts', 'TotLen Bwd Pkts', 'Fwd Pkt Len Max',
+    'Fwd Pkt Len Min', 'Fwd Pkt Len Mean', 'Fwd Pkt Len Std',
+    'Bwd Pkt Len Max', 'Bwd Pkt Len Min', 'Bwd Pkt Len Mean',
+    'Bwd Pkt Len Std', 'Flow Byts/s', 'Flow Pkts/s', 'Flow IAT Mean',
+    'Flow IAT Std', 'Flow IAT Max', 'Flow IAT Min', 'Fwd IAT Tot',
+    'Fwd IAT Mean', 'Fwd IAT Std', 'Fwd IAT Max', 'Fwd IAT Min',
+    'Bwd IAT Tot', 'Bwd IAT Mean', 'Bwd IAT Std', 'Bwd IAT Max',
+    'Bwd IAT Min', 'Fwd PSH Flags', 'Bwd PSH Flags', 'Fwd URG Flags',
+    'Bwd URG Flags', 'Fwd Header Len', 'Bwd Header Len', 'Fwd Pkts/s',
+    'Bwd Pkts/s', 'Pkt Len Min', 'Pkt Len Max', 'Pkt Len Mean',
+    'Pkt Len Std', 'Pkt Len Var', 'FIN Flag Cnt', 'SYN Flag Cnt',
+    'RST Flag Cnt', 'PSH Flag Cnt', 'ACK Flag Cnt', 'URG Flag Cnt',
+    'CWE Flag Count', 'ECE Flag Cnt', 'Down/Up Ratio', 'Pkt Size Avg',
+    'Fwd Seg Size Avg', 'Bwd Seg Size Avg', 'Fwd Byts/b Avg',
+    'Fwd Pkts/b Avg', 'Fwd Blk Rate Avg', 'Bwd Byts/b Avg',
+    'Bwd Pkts/b Avg', 'Bwd Blk Rate Avg', 'Subflow Fwd Pkts',
+    'Subflow Fwd Byts', 'Subflow Bwd Pkts', 'Subflow Bwd Byts',
+    'Init Fwd Win Byts', 'Init Bwd Win Byts', 'Fwd Act Data Pkts',
+    'Fwd Seg Size Min', 'Active Mean', 'Active Std', 'Active Max',
+    'Active Min', 'Idle Mean', 'Idle Std', 'Idle Max', 'Idle Min'
 ]
 
 # X_COLS = [
@@ -159,6 +163,24 @@ if USE_SOURCE_DATA:
 
 # sample the dataset to make it smaller
 dataset = dataset.sample(frac=DATASET_SAMPLE_PERCENT)
+
+# group by timestamp in 10 second intervals
+dataset[TIMESTAMP_COL] = pd.to_datetime(dataset[TIMESTAMP_COL], dayfirst=True)
+dataset['ts_interval'] = dataset[TIMESTAMP_COL].dt.floor(SAMPLING_INTERVAL)
+
+# aggregate the data
+dataset = dataset.groupby(['ts_interval'], dropna=True).agg({
+    TIMESTAMP_COL: 'first',
+    'Protocol': 'first',
+    'Label': 'max',
+    'Dst Port': 'first',
+    'Dst IP': 'first',
+    'Src Port': 'first',
+    'Src IP': 'first',
+    'Flow ID': 'first',
+    # the rest of the columns are averaged
+    **{col: 'mean' for col in dataset.columns if col not in [TIMESTAMP_COL, 'Protocol', 'Label', 'Dst Port', 'Dst IP', 'Src Port', 'Src IP', 'Flow ID']}
+}).reset_index(drop=True)
 
 # print the first few rows of the dataset
 logger.info(dataset.head())
